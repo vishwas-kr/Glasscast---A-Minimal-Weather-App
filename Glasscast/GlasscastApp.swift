60a3b286c6d054366784349c8de2c751eed5d007
kr-vishwas <vishwaskumar445@gmail.com>
2026-01-18 21:36:31 +0530

    Initial Commit

---
+++ Glasscast/GlasscastApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlasscastApp.swift
+//  Glasscast
+//
+//  Created by vishwas on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct GlasscastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
